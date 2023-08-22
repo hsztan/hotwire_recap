d67f70d2434fd6cbbc765833bca27413ebc740c0
Henry Nawrocki <henry@sztanski.com>
2023-08-22 17:44:56 -0500

    create route to create programming languages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get   'page2',  to: 'application#page2'
   post  'page3',  to: 'application#page3'
   get   'page4',  to: 'application#page4'
+
+  get   'programming-languages', to: 'application#programming_languages'
+  post  'programming-language',  to: 'application#create_programming_language'
 end
