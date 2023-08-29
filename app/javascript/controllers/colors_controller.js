1ac4dc9a2d07ae984c27d82bed8dde758034276b
Henry Nawrocki <henry@sztanski.com>
2023-08-29 06:29:00 -0500

    Add targets for stimulus controler to change div color

---
+++ app/javascript/controllers/colors_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from '@hotwired/stimulus';
+
+// Connects to data-controller="colors"
+export default class extends Controller {
+  static targets = ['input', 'card'];
+  connect() {}
+
+  colorizeCard() {
+    this.cardTarget.classList.remove('bg-white');
+    this.cardTarget.style = `background-color: ${this.inputTarget.value}`;
+  }
+}
