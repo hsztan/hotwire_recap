ae439bd76a1505013e49b66440e80e736b2bb1c8
Henry Nawrocki <henry@sztanski.com>
2023-08-29 06:54:58 -0500

    Finish stimulus practice

--- app/javascript/controllers/colors_controller.js
+++ app/javascript/controllers/colors_controller.js
@@ -3,10 +3,17 @@ import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="colors"
 export default class extends Controller {
   static targets = ['input', 'card'];
+  static classes = ['reset'];
+
   connect() {}
 
   colorizeCard() {
     this.cardTarget.classList.remove('bg-white');
     this.cardTarget.style = `background-color: ${this.inputTarget.value}`;
+  }
+
+  resetCard() {
+    this.cardTarget.style = '';
+    this.cardTarget.classList.add(this.resetClass);
   }
 }
